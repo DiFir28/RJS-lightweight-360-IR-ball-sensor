e80ad78c8f28ed7a25edb9614c834e3dd5e9d5a1
difir <28102008dima@gmail.com>
2025-11-28 20:52:30 +0300

    16_sensors_update

--- RJS-IR-sensor/RJS-IR-sensor.ino
+++ RJS-IR-sensor/RJS-IR-sensor.ino
@@ -1,22 +1,29 @@
+#include <Arduino.h>
 #include <Wire.h>
 
-int SensorPins[] = {15, 14, 29, 28, 27, 26, 24, 23, 22, 5, 6, 2, 17, 7, 16, 9, 10, 11, 12, 35, 34, 32, 31, 30};
-int rersensorpins[] = {40, 41, 7, 6, 5, 4, 2, 1, 0, 27, 35, 28, 32, 36, 33, 38, 12, 13, 14, 18, 19, 21, 22, 23};
-unsigned long readEE[200] = {0};
-unsigned long raw_valueABC[200] = {0};
-unsigned long raw_valueEHJ[200] = {0};
+#define printf(x) Serial.print(x);
+#define printfs(x) Serial.print(x); Serial.print(" ");
+#define printflt(x) Serial.print(x); Serial.print("\t");
+#define printfln(x) Serial.println(x);
+
+int SensorPins[] = {4,3,2,13,  12,A3,A2,A1,  A0,11,10,9,  8,7,6,5   };
+int rersensorpins[] = {20,19,18,5,  4,11,10,9,  8,3,2,1,  0,23,22,21 };
+int err_pins[] = {0,0,0,1,  0,0,0,0,  0,0,0,0,  0,0,0,0}; 
+int readEE[16] = {0};
+unsigned long raw_valueBCD[300] = {0};
 
 short ir, rast = 0, qrast = 0, max_ = 0;
 double cos_side, sin_side;
 float angle = 0;
 int data[2];
-unsigned long pinc = 0, pinj = 0, one = 1;
+unsigned long pind = 0,  one = 1;
 
-float _err_measure = 3;  // примерный шум измерений
-float _q = 0.2;   // скорость изменения значений 0.001-1, варьировать самому
+float _err_measure = 3;  
+float _q = 0.2;   
 
 int b = 12, max_i  = 0;
 float a = 0;
+
 float simpleKalman(float newVal) {
   float _kalman_gain, _current_estimate;
   static float _err_estimate = _err_measure;
@@ -38,56 +45,46 @@ void setup() {
   Serial.begin(115200);
   Wire.begin(0x09);
   Wire.onRequest(sendData);
-  for (int i = 0; i < 40; ++i) {
-    pinMode(i, INPUT);
+  for (int i = 0; i < 16; ++i) {
+    pinMode(SensorPins[i], INPUT);
   }
-  for (int i = 0; i < 24; i++) {
+  for (int i = 0; i < 16; i++) {
     readEE[0] = 0;
   }
 
 }
 
 void loop() {
-  cos_side = 0, sin_side = 0, rast = 0, max_ =  0;;
-
+  
+  cos_side = 0, sin_side = 0, rast = 0, max_ =  0;
   ir = 0;
-  //Serial.print("---");
+
   unsigned long startTime_us = micros() ;
-  pinc = PINC;
-  pinj = PINJ;
-  raw_valueABC [ir] = PINA + (PINB << 8) + (pinc << 16);
-  raw_valueEHJ [ir] = PINE + (PINH << 8) + (PINJ << 16);
-  //  Serial.print((micros() - startTime_us));
-  //  Serial.print("    ");
+
+  pind = PIND;
+  raw_valueBCD [ir] = PINB + (PINC << 8 + (pind << 16);
   startTime_us = micros() ;
 
-  while ((micros() - startTime_us) < 833) {
-    pinc = PINC;
-    pinj = PINJ;
-    raw_valueABC [ir] = PINA + (PINB << 8) + (pinc << 16);
-    raw_valueEHJ [ir] = PINE + (PINH << 8) + (pinj << 16);
-
+  while ((micros() - startTime_us) < 833*2) {
+    pind = PIND;
+    raw_valueBCD [ir] = PINB + (PINC << 8 + (pind << 16);
     ir += 1;
   }
-  //
-  //  Serial.print(ir);
-  //  Serial.print("   ");
 
-  for (int i = 0; i < 24; i++) {
+
+  for (int i = 0; i < 16; i++) {
     int shift = rersensorpins[i];
-    if (shift < 24) {
-      for (int j = 0; j < ir; j++) {
-        readEE[i] += ((raw_valueABC[j] & (one << shift)) == 0);
-      }
-    } else {
-      shift -= 24;
-      for (int j = 0; j < ir; j++) {
-        readEE[i] += ((raw_valueEHJ[j] & (one << shift)) == 0);
-      }
+    for (int j = 0; j < ir-1; j++) {
+      readEE[i] += ((raw_valueBCD[j] & (one << shift)) == 0);
     }
   }
+
+
   int rett = 0;
-  for (int i = 0; i < 24; i++) {
+  for (int i = 0; i < 16; i++) {
+    if (err_pins[i]){
+      continue;
+    }
     rett += (readEE[i] > 0);
     cos_side += (cos(i * 15.0 * 3.1415926 / 180.0) * readEE[i]  * 100 );
     sin_side += (sin(i * 15.0 * 3.1415926 / 180.0) * readEE[i] * 100 );
@@ -96,35 +93,30 @@ void loop() {
       max_i = i;
     }
   }
-  Serial.print(rett);
-  Serial.print("   ");
+
+
+  printflt(ir);
   angle = simpleKalman((atan2(sin_side, cos_side) * 180 / 3.1415));
-  Serial.print(angle);
-  Serial.print("   ");
+  printflt(angle);
+  
 
-  //  Serial.print(max_);
-  //  Serial.print("   ");
-  a = a + 0.03 * (max_ - min(readEE[max_i + 1], readEE[(max_i + 23)%24 ]) - a);
-  Serial.print(a);
-  Serial.print("   ");
-
-  for (int i = 0; i < 24; i++) {
-        Serial.print(readEE[i]);
-        Serial.print(" ");
+  for (int i = 0; i < 16; i++) {
+    printfs(readEE[i]);
     readEE[i] = 0;
   }
-  for (int i = 0; i < 200; i++) {
-
-    raw_valueABC [i] = 0;
-    raw_valueEHJ [i] = 0;
+  for (int i = 0; i < ir+1; i++) {
+    raw_valueBCD [i] = 0;
   }
+
+  
   if (rett  == 0) {
     data[0] = 999;
     data[1] = -1;
-    } else {
+  } else {
     data[0] = angle;
     data[1] = a * 100;
   }
 
-  Serial.println("");
+
+  printfln("");
 }
